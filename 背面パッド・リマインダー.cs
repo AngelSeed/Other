742615e81ad2241ab600a73a31af15465f401892
Wings <51082108+AngelSeed@users.noreply.github.com>
2024-05-12 09:04:47 +0900

    Add files via upload
    背面パッド用

---
+++ 背面パッド・リマインダー.cs
@@ -0,0 +1,38 @@
+using Dalamud.Game.Text.SeStringHandling;
+using ECommons;
+using ECommons.ChatMethods;
+using ECommons.DalamudServices;
+using ECommons.Hooks;
+using ECommons.Schedulers;
+using Splatoon.SplatoonScripting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplatoonScriptsOfficial.Generic
+{
+    public class 背面パッド用 : SplatoonScript
+    {
+        public override HashSet<uint> ValidTerritories => new();
+        public override Metadata? Metadata => new(1, "Angel Wings XIV JP.Version");
+
+        public override void OnDirectorUpdate(DirectorUpdateCategory category)
+        {
+            if(category.EqualsAny(DirectorUpdateCategory.Commence, DirectorUpdateCategory.Recommence))
+            {
+                if(!Process.GetProcessesByName("SUPPORT_GAMEPAD_FFXIV,1").Any())
+                {
+                    var s = new SeStringBuilder().AddUiForeground("※WARNING※ 背面パッドが使えないぞ！ ※WARNING※", (ushort)UIColor.Red).Build();
+                    for (var i = 0; i < 1; i++)
+                    {
+                        Svc.Chat.Print(s);
+                    }
+                    Svc.Toasts.ShowError(s.ExtractText());
+                }
+            }
+        }
+    }
+}
