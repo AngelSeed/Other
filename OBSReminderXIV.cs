066426041a53dd533af687efd6d16c53b786d696
Wings <51082108+AngelSeed@users.noreply.github.com>
2026-04-20 20:46:45 +0900

    Add files via upload
    OBSReminderXIV

---
+++ OBSReminderXIV.cs
@@ -0,0 +1,38 @@
+using Dalamud.Game.Text.SeStringHandling;
+using ECommons;
+using ECommons.ChatMethods;
+using ECommons.DalamudServices;
+using ECommons.Hooks;
+using ECommons.Schedulers;
+using Splatoon.SplatoonScripting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplatoonScriptsOfficial.Generic
+{
+    public class ActReminderXIV : SplatoonScript
+    {
+        public override HashSet<uint> ValidTerritories => new();
+        public override Metadata? Metadata => new(1, "Angel Wings XIV JP.Version");
+
+        public override void OnDirectorUpdate(DirectorUpdateCategory category)
+        {
+            if(category.EqualsAny(DirectorUpdateCategory.Commence, DirectorUpdateCategory.Recommence))
+            {
+                if(!Process.GetProcessesByName("OBS").Any())
+                {
+                    var s = new SeStringBuilder().AddUiForeground("※WARNING※ OBSが起動してないよ！ ※WARNING※", (ushort)UIColor.Red).Build();
+                    for (var i = 0; i < 1; i++)
+                    {
+                        Svc.Chat.Print(s);
+                    }
+                    Svc.Toasts.ShowError(s.ExtractText());
+                }
+            }
+        }
+    }
+}
